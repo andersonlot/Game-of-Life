8a539a6283c11cd9ad70711ca4eb97f4b1662dba
andersonlot <andersonlot.ee@gmail.com>
2022-08-31 09:56:51 -0300

    Adicionado matriz com as células e possibilidade de clicar com mouse para "viver" uma célula

--- game of life/sketch.js
+++ game of life/sketch.js
@@ -1,9 +1,38 @@
+var celulas=[];
+var tamanho_celulas=10;
+var linhas;
+var colunas;
+
 function setup() {
-  // put setup code here
-  createCanvas(500,500);
+  createCanvas(720,480);
+  linhas=height/tamanho_celulas;
+  colunas=width/tamanho_celulas;
+  for(let i=0;i<linhas;i++){
+    celulas[i]=[];
+    for(let j=0;j<colunas;j++){
+      celulas[i][j]=false;
+    }
+  }
+  print(celulas);
 }
 
 function draw() {
-  // put drawing code here
-  background(0,30,0);
+  background(40);
+  for(let i=0;i<linhas;i++){
+    for(let j=0;j<colunas;j++){
+      stroke(55);
+      if(celulas[i][j]){
+        fill(240);
+      }else{
+        noFill();
+      }
+      rect(j*tamanho_celulas,i*tamanho_celulas,tamanho_celulas);
+    }
+  }
+}
+
+function mousePressed(){
+  let celulaX=floor(mouseX/tamanho_celulas);
+  let celulaY=floor(mouseY/tamanho_celulas);
+  celulas[celulaY][celulaX]=true;
 }
